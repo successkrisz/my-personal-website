19a9384470c5faa9cac2aecc880ae0ce415ca9c1
Krisztian Balla <kriszi.balla@gmail.com>
2017-02-21 09:27:39 +0000

    feat(footer): add global Footer component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,39 @@
+import React, { PropTypes } from 'react';
+import { Grid, Row, Col } from 'react-bootstrap';
+import './Footer.css';
+
+const Footer = ({ github, linkedin, quote }) => (
+  <footer>
+    <Grid>
+      <Row>
+        <Col xs={12} sm={8}>
+          <p><i className='fa fa-quote-left' aria-hidden='true' />
+            {quote.text}
+            <i className='fa fa-quote-right' aria-hidden='true' /></p>
+          <div className='text-right italic'> - {quote.author}</div>
+        </Col>
+        <Col xs={12} sm={4}>
+          <a href={github} target='_blank'>
+            <i className='fa fa-github fa-3x' aria-hidden='true' /> GitHub
+          </a>
+          <br />
+          <a href={linkedin} target='_blank'>
+            <i className='fa fa-linkedin-square fa-3x' aria-hidden='true' /> LinkedIn
+          </a>
+          <br />
+          <a href=''>
+            <i className='fa fa-file-pdf-o fa-3x' aria-hidden='true' /> View my CV
+          </a>
+        </Col>
+      </Row>
+    </Grid>
+  </footer>
+);
+
+Footer.propTypes = {
+  github: PropTypes.string.isRequired,
+  linkedin: PropTypes.string.isRequired,
+  quote: PropTypes.object.isRequired
+};
+
+export default Footer;
