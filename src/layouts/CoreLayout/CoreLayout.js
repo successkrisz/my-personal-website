912f539a1eef6635c81e266cb9a3a91ea9b06a7d
Krisztian Balla <kriszi.balla@gmail.com>
2017-02-21 09:31:54 +0000

    feat(layout): add global CoreLayout and bootstrap custom css

---
+++ src/layouts/CoreLayout/CoreLayout.js
@@ -0,0 +1,23 @@
+import React, { PropTypes } from 'react';
+import { Grid } from 'react-bootstrap';
+
+import FooterContainer from 'containers/FooterContainer';
+import Navigation from 'components/Navigation';
+import 'styles/css/bootstrap.min.css';
+import './CoreLayout.scss';
+
+const CoreLayout = ({ children }) => (
+  <div>
+    <Navigation />
+    <Grid id='content'>
+      { children }
+    </Grid>
+    <FooterContainer />
+  </div>
+);
+
+CoreLayout.propTypes = {
+  children: PropTypes.any
+};
+
+export default CoreLayout;
