c5b52d0a4b6e9c09f60bb6e8af8f59b5dc53487d
Krisztian Balla <kriszi.balla@gmail.com>
2017-03-01 00:47:52 +0000

    refactor(GitHubWidget): move GitHub API fetching to container component

--- src/routes/Home/components/GitHubWidget.js
+++ src/routes/Home/components/GitHubWidget.js
@@ -1,52 +1,30 @@
-import React, { Component, PropTypes } from 'react';
+import React, { PropTypes } from 'react';
 import { Col, Panel, Badge } from 'react-bootstrap';
-import axios from 'axios';
 
-class GitHubWidget extends Component {
-  constructor () {
-    super();
-    this.state = {
-      repos: null,
-      fetching: true
-    };
-  }
-  componentDidMount () {
-    const apiUrl = `https://api.github.com/users/${this.props.username}/repos?sort=pushed`;
-    axios.get(apiUrl)
-      .then(json => {
-        const repos = json.data.map(repo => ({
-          id: repo.id,
-          url: repo.html_url,
-          name: repo.name,
-          description: repo.description,
-          pushedAt: repo.pushed_at
-        }));
-        this.setState({ fetching: false, repos });
-      });
-  }
-  render () {
-    const numberOfRepos = (this.state.fetching) ? '' : <Badge>{this.state.repos.length}</Badge>;
-    const header = <h3><a href={`https://github.com/${this.props.username}`} target='_blank'>
-                      My <i className='fa fa-github' /> Repositories {numberOfRepos}</a></h3>;
-    return (
-      <Col xs={12} sm={6}>
-        <Panel header={header}>
-          <ul>
-            {(this.state.fetching) ? <i className='fa fa-spinner' />
-            : this.state.repos.map(repo => (
-              <li key={repo.id}>
-                <a href={repo.url} target='_blank'>{repo.name}</a>
-                <p>{repo.description}</p>
-              </li>
-            ))}
-          </ul>
-        </Panel>
-      </Col>
-    );
-  }
-}
+const GitHubWidget = ({ fetching, repos, username }) => {
+  const numberOfRepos = (fetching) ? '' : <Badge>{repos.length}</Badge>;
+  const header = <h3><a href={`https://github.com/${username}`} target='_blank'>
+                    My <i className='fa fa-github' /> Repositories {numberOfRepos}</a></h3>;
+  return (
+    <Col xs={12} sm={6}>
+      <Panel header={header}>
+        <ul>
+          {(fetching) ? <i className='fa fa-spinner' />
+          : repos.map(repo => (
+            <li key={repo.id}>
+              <a href={repo.url} target='_blank'>{repo.name}</a>
+              <p>{repo.description}</p>
+            </li>
+          ))}
+        </ul>
+      </Panel>
+    </Col>
+  );
+};
 
 GitHubWidget.propTypes = {
+  fetching: PropTypes.bool.isRequired,
+  repos: PropTypes.oneOfType([PropTypes.bool, PropTypes.array]),
   username: PropTypes.string.isRequired
 };
 
--- src/routes/Home/containers/GitHubWidgetContainer.js
+++ src/routes/Home/containers/GitHubWidgetContainer.js
@@ -1,7 +1,40 @@
+import React, { Component, PropTypes } from 'react';
+import axios from 'axios';
 import { connect } from 'react-redux';
 
 import GitHubWidget from '../components/GitHubWidget';
 
+class GitHubWidgetContainer extends Component {
+  constructor () {
+    super();
+    this.state = {
+      repos: null,
+      fetching: true
+    };
+  }
+  componentDidMount () {
+    const apiUrl = `https://api.github.com/users/${this.props.username}/repos?sort=pushed`;
+    axios.get(apiUrl)
+      .then(json => {
+        const repos = json.data.map(repo => ({
+          id: repo.id,
+          url: repo.html_url,
+          name: repo.name,
+          description: repo.description,
+          pushedAt: repo.pushed_at
+        }));
+        this.setState({ fetching: false, repos });
+      });
+  }
+  render () {
+    return <GitHubWidget {...this.state} username={this.props.username} />;
+  }
+}
+
+GitHubWidgetContainer.propTypes = {
+  username: PropTypes.string.isRequired
+};
+
 const mapStateToProps = state => ({ username: state.data.github.username });
 
-export default connect(mapStateToProps)(GitHubWidget);
+export default connect(mapStateToProps)(GitHubWidgetContainer);
