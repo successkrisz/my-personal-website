b374c0cb0009b227bb3b78638cc627abfefa67c0
Krisztian Balla <kriszi.balla@gmail.com>
2017-03-06 09:17:39 +0000

    refactor(api): use async functions for api calls

--- src/lib/api.js
+++ src/lib/api.js
@@ -5,32 +5,24 @@ const APIS = {
   github: 'https://api.github.com/users/successkrisz/repos?sort=pushed'
 };
 
-export const fetchHomePageContent = () => {
-  return new Promise(resolve => {
-    firebaseRef.once('value')
-    .then(snapshot => resolve(snapshot.val()));
-  });
+export const fetchHomePageContent = async () => {
+  return firebaseRef.once('value')
+    .then(snapshot => snapshot.val());
 };
 
-export const fetchGitHubAPI = () => {
-  return new Promise(resolve => {
-    axios.get(APIS.github)
+export const fetchGitHubAPI = async () => {
+  return axios.get(APIS.github)
     .then(response => {
-      const repos = response.data.map(repo => ({
+      return response.data.map(repo => ({
         id: repo.id,
         url: repo.html_url,
         name: repo.name,
         description: repo.description,
         pushedAt: repo.pushed_at
       }));
-      resolve(repos);
     });
-  });
 };
 
-export const sendMessage = message => {
-  return new Promise(resolve => {
-    firebaseRef.child('messages').push(message)
-    .then(() => resolve());
-  });
+export const sendMessage = async (message) => {
+  return firebaseRef.child('messages').push(message)
 };
