de066bdf3afc133ee3366b555cedfc33fc637e22
Krisztian Balla <kriszi.balla@gmail.com>
2017-03-12 15:38:42 +0000

    fix(tests): add tests to CoreLayout to cover Header Component

--- tests/layouts/CoreLayout.spec.js
+++ tests/layouts/CoreLayout.spec.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+
 import CoreLayout from 'layouts/CoreLayout';
 import Navigation from 'components/Navigation';
 import FooterContainer from 'containers/FooterContainer';
+import Header from 'components/Header';
 
 describe('(Layout) Core', () => {
   let _wrapper;
 
   it('Should render a spinner if data isn\'t supplied', () => {
-    _wrapper = shallow(<CoreLayout><h1>children</h1></CoreLayout>);
+    _wrapper = shallow(<CoreLayout data={false}><h1>children</h1></CoreLayout>);
 
     expect(_wrapper.contains(
       <i className='fa fa-spinner fa-spin' />
@@ -25,6 +27,7 @@ describe('(Layout) Core', () => {
     });
 
     it('Should render Navigation, Children and Footer', () => {
+      expect(_wrapper.find(Header)).to.have.length(1);
       expect(_wrapper.contains(<Navigation />)).to.be.true;
       expect(_wrapper.contains(<FooterContainer />)).to.be.true;
       expect(_wrapper.contains(<h1>children</h1>)).to.be.true;
